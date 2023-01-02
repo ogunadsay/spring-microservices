2bc7b8c6bdaa7660e7bc2213d646d990748dcec2
Ogun Adsay <ogun.adsay@emakina.com.tr>
2023-01-02 14:28:15 +0300

    pagination

--- src/main/java/com/epam/mentoring/microservices/repository/TourPackageRepository.java
+++ src/main/java/com/epam/mentoring/microservices/repository/TourPackageRepository.java
@@ -2,11 +2,10 @@ package com.epam.mentoring.microservices.repository;
 
 import com.epam.mentoring.microservices.domain.TourPackage;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.lang.Nullable;
+import org.springframework.data.repository.query.Param;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface TourPackageRepository extends CrudRepository<TourPackage, String> {
-    Optional<TourPackage> findByNameIgnoreCase(String name);
+    Optional<TourPackage> findByNameIgnoreCase(@Param("name") String name);
 }
--- src/main/java/com/epam/mentoring/microservices/repository/TourRepository.java
+++ src/main/java/com/epam/mentoring/microservices/repository/TourRepository.java
@@ -1,11 +1,12 @@
 package com.epam.mentoring.microservices.repository;
 
 import com.epam.mentoring.microservices.domain.Tour;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 
-import java.util.List;
-
-public interface TourRepository extends CrudRepository<Tour, Integer> {
-    List<Tour> findByTourPackageCode(@Param("code") String code);
+public interface TourRepository extends CrudRepository<Tour, Integer>, PagingAndSortingRepository<Tour, Integer> {
+    Page<Tour> findByTourPackageCode(@Param("code") String code, Pageable pageable);
 }
